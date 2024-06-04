5b3bbf3b7fe1a25a7f806d2dad807d7510482648
Sergei Dmitriev <iserdmi@gmail.com>
2024-06-04 14:11:32 +0400

    Small fix

--- src/client.tsx
+++ src/client.tsx
@@ -1,25 +1,41 @@
 import { createErroryThings, type ErroryType } from 'errory'
 import type { PageContextServer } from 'vike/types'
 
-export const createVikeClientThings = <TErroryType extends ErroryType>({ Errory }: { Errory?: TErroryType } = {}) => {
+export const createVikeClientThings = <
+  TErroryType extends ErroryType,
+  TDataGetterResultExtra extends Record<string, any> = {},
+>({
+  Errory,
+  dataGetterResultExtender = () => ({}) as TDataGetterResultExtra,
+}: {
+  Errory?: TErroryType
+  dataGetterResultExtender?: (pageContext: PageContextServer) => TDataGetterResultExtra
+}) => {
   Errory = Errory || (createErroryThings().Errory as TErroryType)
 
   const createDataGetter = <TData,>(
     dataGetter: (pageContext: PageContextServer) => Promise<TData>
   ): ((pageContext: PageContextServer) => Promise<
-    | TData
-    | {
+    | (TData & TDataGetterResultExtra)
+    | ({
         dataGetterError: ReturnType<TErroryType['toErrory']>
-      }
+      } & TDataGetterResultExtra)
   >) => {
     return (async (pageContext: PageContextServer) => {
-      try {
-        return await dataGetter(pageContext)
-      } catch (error) {
-        return {
-          dataGetterError: Errory.toErrory(error),
+      const result = await (async () => {
+        try {
+          return await dataGetter(pageContext)
+        } catch (error) {
+          return {
+            dataGetterError: Errory.toErrory(error),
+          }
         }
-      }
+      })()
+      const extra = dataGetterResultExtender(pageContext)
+      return {
+        ...result,
+        ...extra,
+      } as any
     }) as any
   }
 
